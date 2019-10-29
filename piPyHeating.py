b4c4a565d04e623b334925fb7573eccf1702f37b
Walt <brian@riban.co.uk>
2019-10-29 18:52:15 +0000

    Adds dictionary for sensors

--- piPyHeating.py
+++ piPyHeating.py
@@ -12,6 +12,7 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 roomSensor = '28-0000053f0ba3'
+cylinderSensor = '28-000005a2a817'
 button = Button(27, True, None, 0.05, 0)
 led = PWMLED(22, False)
 state = 0
@@ -20,26 +21,34 @@ currentTemp = {roomSensor: 0}
 roomSetTemp = 21.5
 cylinderSetTemp = 55
 
+# Dictionary of DSB18S20 temperature sensors
+sensors = {
+	"room": {"id": "28-0000053f0ba3", "name": "Hall",  "value": 0},
+	"cylinder": {"id": "28-000005a2a817", "name": "Water", "value": 0}
+}
+
 # Dictionary contains scheduled events (on, off, bitwise days - Monday = 1, Sunday = 64)
 schedule = {
 	"timer1": { "on": 360, "off": 600, "days": 127},
 	"timer2": {"on": 900, "off": 1350, "days": 127},
 }
 
-# Get raw temeperature sensor value
+# Get raw temeperature sensor value from sensor name (room / cylinder)
 def getRawTemp(sensor):
-	tempSensor = '/sys/bus/w1/devices/' + sensor + '/w1_slave'
+	tempSensor = '/sys/bus/w1/devices/' + sensors[sensor]["id"] + '/w1_slave'
 	try:
 		f = open(tempSensor, 'r')
 		lines = f.readlines()
 		f.close()
 		return lines
 	except:
-		print("Sensor " + sensor + " error")
+		print("Sensor " + sensors[sensor][name] + " error")
 
 # Get temperature sensor value in degrees Celsius
 def getTemp(sensor):
 	lines = getRawTemp(sensor)
+	if lines == None:
+		return
 	while lines[0].strip()[-3:] != 'YES':
 		time.sleep(0.2)
 		lines = getRawTemp(sensor)
@@ -48,9 +57,9 @@ def getTemp(sensor):
 
 	if temp_output != -1:
 		temp_string = lines[1].strip()[temp_output+2:]
-		temp_c = float(temp_string) / 1000.0
+		temp_c = round(float(temp_string) / 1000.0, 1)
 		if temp_c != 85:
-			currentTemp[sensor] = temp_c
+			sensors[sensor]["value"] = temp_c
 
 # Handle button event
 def onButton():
@@ -82,7 +91,7 @@ def processTemp():
 			pass
 		led.off()
 	if state == 1:
-		if currentTemp[roomSensor] > roomSetTemp:
+		if sensors["room"]["value"] > roomSetTemp:
 			try:
 				pump.close()
 				boiler.close()
@@ -96,12 +105,12 @@ def processTemp():
 			except:
 				pass
 			led.blink()
-	print now.time(), state, currentTemp[roomSensor]
+	print now.strftime("%H:%M:%S %d/%m/%Y"), state, sensors["room"]["value"]
 	return 60 - now.time().second
 
 # Handle alarm signal (triggered on minute boundary)
 def onAlarm(signum, frame):
-	getTemp(roomSensor)
+	getTemp("room")
 	alarm(processTemp())
 
 # Register for button events
