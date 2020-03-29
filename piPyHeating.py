f60d5a1276263e2afd8b240c40e18717e50fb95d
Walt <brian@riban.co.uk>
2020-03-29 11:28:54 +0100

    Use tornado web

--- piPyHeating.py
+++ piPyHeating.py
@@ -1,3 +1,7 @@
+# riban piPyHeating.py
+# (c) riban 2020
+# Provides timer control of heating pump and boiler
+
 import os
 import time
 from gpiozero import Button
@@ -7,7 +11,8 @@ from signal import pause
 from signal import alarm
 import signal
 from datetime import datetime
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import tornado.web
+import tornado.ioloop
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -25,9 +30,34 @@ sensors = {
 
 # Dictionary contains scheduled events (on, off, bitwise days - Monday = 1, Sunday = 64)
 schedule = {
-	"timer1": { "on": 360, "off": 600, "days": 127},
+	"timer1": { "on": 360, "off": 600, "days": 63},
 	"timer2": {"on": 900, "off": 1350, "days": 127},
+	"timer3": { "on": 420, "off": 720, "days": 64},
 }
+
+# Create a web handler
+def make_app():
+	settings = {"template_path": "templates"}
+	return tornado.web.Application([
+		(r'/$', HomeHandler)
+	], **settings)
+
+# Handle web requests
+class HomeHandler(tornado.web.RequestHandler):
+	def get(self):
+		action = self.get_argument('action', None)
+		if action != None:
+			if action == 'on':
+				turnOn()
+			elif action == 'off':
+				turnOff()
+			elif action == 'up':
+				incrementRoom(1)
+			elif action == 'down':
+				incrementRoom(-1)
+			self.redirect('/')    # Redirect back to the root url
+			return
+		super().render("home.html", title="riban Heating", sensors=sensors, state=stateName[state])
 
 # Get raw temeperature sensor value from sensor name (room / cylinder)
 def getRawTemp(sensor):
@@ -143,134 +173,19 @@ def onAlarm(signum, frame):
 	getTemp("cylinder")
 	alarm(processTemp())
 
-# Web server
-host_name = 'rpi4.local'
-host_port = 8000
-
-class onHttp(BaseHTTPRequestHandler):
-	def do_HEAD(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
-
-	def _redirect(self, path):
-		self.send_response(303)
-		self.send_header('Content-type', 'text/html')
-		self.send_header('Location', path)
-		self.end_headers()
-
-	def do_GET(self):
-		get_data = self.path.split("=")
-		if len(get_data) > 1:
-			if get_data[1] == 'on':
-				turnOn()
-			elif get_data[1] == 'off':
-				turnOff()
-			elif get_data[1] == 'up':
-				incrementRoom(1)
-			elif get_data[1] == 'down':
-				incrementRoom(-1)
-			self._redirect('/')    # Redirect back to the root url
-			return
-		html = '''
-<html>
-	<meta content='width=device-width, initial-scale=1' name='viewport'/>
-	<head><style>
-		.on {{
-			height: 20%;
-			font-size: 6vw;
-			background-color: #339966;
-			color: white;
-			text-align: center;
-			cursor: pointer;
-			border-style: solid;
-			display: table-cell;
-			vertical-align: middle;
-		}}
-		.off {{
-			height: 20%;
-			font-size: 6vw;
-			background-color: #333333;
-			color: white;
-			text-align: center;
-			cursor: pointer;
-			border-style: solid;
-			display: table-cell;
-			vertical-align: middle;
-		}}
-		.value {{
-			height: 8%;
-			font-size: 5vw;
-			background-color: black;
-			color: white;
-			text-align: center;
-			border-style: solid;
-			display: table-cell;
-			vertical-align: middle;
-		}}
-		.parent {{
-			display: table;
-			height: 100%;
-			width: 100%;
-		}}
-		.row {{
-			display: table-row;
-		}}
-
-	</style></head>
-	<body>
-		<div class="parent">
-			<div class="row"><div class = "value">Current: {}&deg;</div></div>
-			<div class="row"><div class="{}"; onclick="location.href='?action=on'";>ON</div></div>
-			<div class="row"><div class="{}"; onclick="location.href='?action=off'";>OFF</div></div>
-			<div class="row"><div class = "value">Requested: {}&deg;</div></div>
-			<div class="row"><div class="on"; onclick="location.href='?action=up'";>UP</div></div>
-			<div class="row"><div class="on"; onclick="location.href='?action=down'";>DOWN</div></div>
-		</div>
-	</body>
-</html>
-		'''
-		temp = sensors["room"]["value"]
-		self.do_HEAD()
-		on="off"
-		off="on"
-		if state:
-			on="on"
-			off="off"
-		self.wfile.write(html.format(round(float(sensors["room"]["value"])/10,2), on, off, float(sensors["room"]["setPoint"])/10).encode("utf-8"))
-
-	def do_POST(self):
-		content_length = int(self.headers['Content-Length'])    # Get the size of data
-		post_data = self.rfile.read(content_length).decode("utf-8")   # Get the data
-		post_data = post_data.split("=")[1]    # Only keep the value
-
-		if post_data == 'On':
-			turnOn()
-		elif post_data == 'Off':
-			turnOff()
-		elif post_data == 'Up':
-			incrementRoom(1)
-		elif post_data == 'Down':
-			incrementRoom(-1)
-		self._redirect('/')    # Redirect back to the root url
-
-# Register for button events
-button.when_pressed = onButton
-
-# Configure alarm signal handler
-signal.signal(signal.SIGALRM, onAlarm)
-
-# Trigger alarm at startup
-alarm(1)
-
-http_server = HTTPServer(('rpi4.local', 8000), onHttp)
-try:
-	http_server.serve_forever()
-except KeyboardInterrupt:
-	http_server.server_close()
-
 # Main loop - just wait for a signal
-while True:
-	pause()
+if __name__ == "__main__":
+	app = make_app()
+	app.listen(8000)
+
+	# Register for button events
+	button.when_pressed = onButton
+
+	# Configure alarm signal handler
+	signal.signal(signal.SIGALRM, onAlarm)
 
 
+	# Trigger alarm at startup
+	alarm(1)
+
+	tornado.ioloop.IOLoop.current().start()
